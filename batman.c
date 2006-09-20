3e8041d39f708c8af585e655b5da6fa5b4024a1b
Marek Lindner <lindner_marek@yahoo.de>
2006-09-20 19:58:53 +0200

    retransmit packets with unidirectional flag if neighbour && not router III

--- batman.c
+++ batman.c
@@ -1024,7 +1024,7 @@ int batman()
 					   to tell him that we get its packets */
 					/* if a bidirectional neighbour sends us a packet - retransmit it with unidirectional flag
 					if it is not our best link to it in order to prevent routing problems */
-					if ( ( in.orig == neigh ) && ( ( !isBidirectionalNeigh( orig_neigh_node ) ) || ( orig_node->router != 0 ) ) ) {
+					if ( ( in.orig == neigh ) && ( ( !isBidirectionalNeigh( orig_neigh_node ) ) || ( orig_neigh_node->router != 0 ) ) ) {
 
 						schedule_forward_packet(&in, 1, orig_neigh_node, neigh);
 
